eea7a02a63f44cf32b6b1ace0ef19b558a0ce584
M-kos <koc.melnikov.1985@gmail.com>
2019-07-27 16:35:14 +0700

    Создал класс News Item

--- src/js/news-item.js
+++ src/js/news-item.js
@@ -1,6 +1,38 @@
 class NewsItem {
-    constructor() {
+    constructor(title, description, url, urlImage) {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlImage = urlImage;
+    }
 
+    create() {
+        let div = document.createElement('div');
+        div.className = "news-item";
+
+        let img = document.createElement('img');
+        img.className = "news-item__img";
+        img.src = this.urlImage;
+
+        let h1 = document.createElement('h1');
+        h1.className = "news-item__title";
+        h1.innerHTML = this.title;
+
+        let p = document.createElement('p');
+        p.className = "news-item__description";
+        p.innerHTML = this.description;
+
+        let a = document.createElement('a');
+        a.className = "news-item__link";
+        a.href = this.url;
+
+        div.append(img, h1, p, a);
+
+        this.div = div;
+    }
+
+    insert(element) {
+        element.appendChild(this.div);
     }
 }
 
